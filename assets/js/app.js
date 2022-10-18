1f183c7bc2961f0fba01ba86a10d5f3294e5a46d
CongTruongTran <trancongtruong240401@gmmail.com>
2022-10-18 09:42:17 +0700

    add responsive grid

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+var iconSearch = document.querySelector(".header__mobile--search-icon")
+var Search = document.querySelector(".header__search")
+
+// setTimeout(() => {
+//     iconSearch.addEventListener('click', () => {
+//             Search.style.display = "flex";
+//         // Search.classList.toggle = "showFlex";
+//     })
+// },3000)
